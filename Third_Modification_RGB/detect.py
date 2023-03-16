e0f768a9c60bea2fcfb0732622541b51a1b6fef5
DatNguyenduy <duynguyendat@gmail.com>
2023-03-16 17:58:01 +0800

    'UPDATE'

--- Third_Modification_RGB/detect.py
+++ Third_Modification_RGB/detect.py
@@ -28,9 +28,9 @@ def level_one_detection(w_image,key1,key2,key3):
 
     nr_table = int(nr/2)
     nc_table = int(nc/2)
-    lu_table_B = push_yside(create_luck_up_table(nr_table,nc_table,key1))
-    lu_table_G = push_yside(create_luck_up_table(nr_table,nc_table,key2))
-    lu_table_R = push_yside(create_luck_up_table(nr_table,nc_table,key3))
+    lu_table_B = create_luck_up_table(nr_table,nc_table,key1)
+    lu_table_G = create_luck_up_table(nr_table,nc_table,key2)
+    lu_table_R = create_luck_up_table(nr_table,nc_table,key3)
     level_one_BGR = np.ones((nr_table,nc_table,3),dtype = np.bool)
     level_one_YCrCb = np.ones((nr_table,nc_table,3),dtype = np.bool)
     for ir in range(nr_table):
@@ -55,58 +55,61 @@ def level_one_detection(w_image,key1,key2,key3):
             ex_pB,ex_vB = watermark_extract(B)[-2:]
             ex_pG,ex_vG= watermark_extract(G)[-2:]
             ex_pR,ex_vR = watermark_extract(R)[-2:]
-            if (ex_pB^ex_vB)&(1^ex_pB^au_p_B) == 0:
-                level_one_BGR[ir_co_B,ic_co_B,0] = False 
-                level_one_YCrCb[ir,ic,1] = False     
-            if (ex_pG^ex_vG)&(1^ex_pG^au_p_G) == 0:
-                level_one_BGR[ir_co_G,ic_co_G,1] = False
-                level_one_YCrCb[ir,ic,2] = False 
-            if (ex_pR^ex_vR)&(1^ex_pR^au_p_R) == 0:
-                level_one_BGR[ir_co_R,ic_co_R,2] = False
-                level_one_YCrCb[ir,ic,0] = False 
-
-            # if (ex_pB^ex_vB) == 0:
+            # if (ex_pB^ex_vB)&(1^ex_pB^au_p_B) == 0:
             #     level_one_BGR[ir_co_B,ic_co_B,0] = False 
-            # if (ex_pG^ex_vG) == 0:
-            #     level_one_BGR[ir_co_G,ic_co_G,1] = False    
-            # if (ex_pR^ex_vR) == 0:
+            #     level_one_YCrCb[ir,ic,1] = False     
+            # if (ex_pG^ex_vG)&(1^ex_pG^au_p_G) == 0:
+            #     level_one_BGR[ir_co_G,ic_co_G,1] = False
+            #     level_one_YCrCb[ir,ic,2] = False 
+            # if (ex_pR^ex_vR)&(1^ex_pR^au_p_R) == 0:
             #     level_one_BGR[ir_co_R,ic_co_R,2] = False
+            #     level_one_YCrCb[ir,ic,0] = False 
+
+            if (ex_pB^ex_vB) == 0:
+                level_one_BGR[ir_co_B,ic_co_B,0] = False 
+            if (ex_pG^ex_vG) == 0:
+                level_one_BGR[ir_co_G,ic_co_G,1] = False    
+            if (ex_pR^ex_vR) == 0:
+                level_one_BGR[ir_co_R,ic_co_R,2] = False
     return level_one_BGR,level_one_YCrCb
 
 def level_two_detection(lv1_table):
-    lv1_table_pad = np.pad(lv1_table,1,'constant',constant_values=True)
+    
     lv2_table = lv1_table.copy()
-    list_index_valid = np.where(lv1_table==True)
-    tribles = [[[0,-1],[-1,-1],[-1,0]],[[-1,0],[-1,1],[0,1]],[[0,1],[1,1],[1,0]],[[1,0],[1,-1],[0,-1]]]
-    for i in range(len(list_index_valid[0])):
-        ir = list_index_valid[0][i]
-        ic = list_index_valid[1][i]
-        ipr = ir + 1
-        ipc = ic + 1
-        for trible in tribles: 
-            valid = False
-            for dr,dc in trible:
-                valid = valid or lv1_table_pad[ipr+dr,ipc+dc]
-            if not valid:
-                lv2_table[ir,ic] = False
-                break
+    for channel in range(3):
+        lv1_table_pad = np.pad(lv1_table[:,:,channel],1,'constant',constant_values=True)
+        list_index_valid = np.where(lv1_table[:,:,channel]==True)
+        tribles = [[[0,-1],[-1,-1],[-1,0]],[[-1,0],[-1,1],[0,1]],[[0,1],[1,1],[1,0]],[[1,0],[1,-1],[0,-1]]]
+        for i in range(len(list_index_valid[0])):
+            ir = list_index_valid[0][i]
+            ic = list_index_valid[1][i]
+            ipr = ir + 1
+            ipc = ic + 1
+            for trible in tribles: 
+                valid = False
+                for dr,dc in trible:
+                    valid = valid or lv1_table_pad[ipr+dr,ipc+dc]
+                if not valid:
+                    lv2_table[ir,ic,channel] = False
+                    break
     return lv2_table
 
 def level_three_detection(lv2_table):
-    lv2_table_pad = np.pad(lv2_table,1,'constant',constant_values=True)
     lv3_table = lv2_table.copy()
-    list_index_valid = np.where(lv2_table==True)
-    neighbors = [[0,-1],[-1,-1],[-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1]]
-    for i in range(len(list_index_valid[0])):
-        ir = list_index_valid[0][i]
-        ic = list_index_valid[1][i]
-        ipr = ir + 1
-        ipc = ic + 1
-        count = 0
-        for dr,dc in neighbors:
-            if not lv2_table_pad[ipr+dr,ipc+dc]:
-                count += 1
-                if count>=5:
-                    lv3_table[ir,ic] = False
-                    break
+    for channel in range(3):
+        lv2_table_pad = np.pad(lv2_table[:,:,channel],1,'constant',constant_values=True)
+        list_index_valid = np.where(lv2_table[:,:,channel]==True)
+        neighbors = [[0,-1],[-1,-1],[-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1]]
+        for i in range(len(list_index_valid[0])):
+            ir = list_index_valid[0][i]
+            ic = list_index_valid[1][i]
+            ipr = ir + 1
+            ipc = ic + 1
+            count = 0
+            for dr,dc in neighbors:
+                if not lv2_table_pad[ipr+dr,ipc+dc]:
+                    count += 1
+                    if count>=5:
+                        lv3_table[ir,ic,channel] = False
+                        break
     return lv3_table 
